152e13a9e8edae50a9ddcfb1d62e3996692899b5
Aarohi-S05 <johari.aarohi2210@gmail.com>
2026-02-17 10:12:44 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+    /**
+     *
+     *
+     * @Aarohi-S05 args
+     */
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.\n");
+    }
+}
